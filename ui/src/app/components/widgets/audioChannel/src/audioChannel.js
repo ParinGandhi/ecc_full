17f1fd53318dadf1d9857a00380962c6aa8f62af
kdiveshr <46379616+kdiveshr@users.noreply.github.com>
2020-03-03 23:11:50 -0500

    changed audio files

--- ui/src/app/components/widgets/audioChannel/src/audioChannel.js
+++ ui/src/app/components/widgets/audioChannel/src/audioChannel.js
@@ -25,8 +25,8 @@ $scope.selectedIndex=10000000;
 $scope.isPushSuccess =false;
 $scope.audioArray=[
 
-{"audioId":"audioTwo","channelName":"Channel one","audioUrl":"http://s-a7564316.mp3pro.xyz/070a3f7059ff4b3c63ce4/Justin%20Bieber%20%E2%80%93%20Despacito%20%F0%9F%8E%A4%20ft.%20Luis%20Fonsi%20%26%20Daddy%20Yankee%20%5BPop%5D.mp3","pannerId":"pannerTwo"},
-{"audioId":"audioThree","channelName":"Channel two","audioUrl":"http://s-905b7872.mp3pro.xyz/86b4178e59ff4b9aa39a2/Jonas%20Blue%20-%20Rise%20ft.%20Jack%20%26%20Jack%20%28%20%29.mp3","pannerId":"pannerTwo"},
+{"audioId":"audioTwo","channelName":"Channel one","audioUrl":"http://streamingv2.shoutcast.com/boleros-para-enamorarse?lang=en-US%2cen%3bq%3d0.9","pannerId":"pannerTwo"},
+{"audioId":"audioThree","channelName":"Channel two","audioUrl":"http://rautemusik-de-hz-fal-stream16.radiohost.de/blackbeats","pannerId":"pannerTwo"},
 {"audioId":"audioFour","channelName":"Channel three","audioUrl":"http://www.music.helsinki.fi/tmt/opetus/uusmedia/esim/a2002011001-e02-128k.mp3","pannerId":"pannerTwo"},
 {"audioId":"audioFive","channelName":"Channel four","audioUrl":"https://files1.mp3slash.xyz/stream/f56fd468db29d4ac24fee86e406a13b2","pannerId":"pannerTwo"},
 {"audioId":"audioSix","channelName":"Channel five","audioUrl":"http://54.39.247.236:4300/?type=http&nocache=287","pannerId":"pannerTwo"}
